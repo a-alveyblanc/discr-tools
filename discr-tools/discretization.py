3d6f3e9bd29ca27c72c9647e3a6e606831aa4e26
Addison Alvey-Blanco <aja13@illinois.edu>
2024-04-11 02:06:39 -0500

    Add global to local map routine

--- discr-tools/discretization.py
+++ discr-tools/discretization.py
@@ -5,8 +5,11 @@ mesh, operators, basis, etc., are all easily accessible.
 """
 
 import numpy as np
+import numpy.linalg as la
 
 import matplotlib.pyplot as plt
+
+from functools import cmp_to_key
 
 from . import geometry as geo
 from .nodal import GLLNodalBasis
@@ -25,9 +28,7 @@ class Discretization:
         self._mapped_elements = geo.map_elements(self._basis_cls.nodes,
                                                  self.mesh.elements)
 
-        dim = self._mapped_elements.shape[0]
-        self._nodes = self._mapped_elements.reshape(dim, -1)
-
+        self.__global_to_local = self._global_to_local()
 
     @property
     def mesh(self):
@@ -42,16 +43,124 @@ class Discretization:
     @property
     def mapped_elements(self):
         return self._mapped_elements
-    
+
+
+    @property
+    def nodes(self):
+        return self._nodes
+
+
+    @property
+    def global_to_local(self):
+        try:
+            return self.__global_to_local
+        except AttributeError:
+            self.__global_to_local = self._global_to_local()
+            return self.__global_to_local
+
 
     @property
     def basis_cls(self):
         return self._basis_cls
 
 
-    @property
-    def nodes(self):
-        return self._nodes
+    def _global_to_local(self):
+        """
+        Establishes global to local node numbering for the case where the number
+        of vertices is smaller than the total number of nodes in an element
+        """
+        elts = self.mapped_elements
+        _, nelts, nnodes = elts.shape
+
+        # {{{ establish local ordering 
+
+        local_ordering = []
+        ipoint = 0
+        for ielt in range(nelts):
+            for inode in range(nnodes):
+                local_ordering.append((ipoint, elts[:,ielt,inode]))
+                ipoint += 1
+
+        def lexicographical_sort(a, b):
+            """
+            Compare two points lexicographically, i.e.
+
+            (0, 0) < (0, 1) < (0, 2) < ... < (0, n) < (1, 0) < ... < (0, 0)
+            """
+
+            if isinstance(a, tuple):
+                a = a[1]
+                b = b[1]
+            
+            if a[0] > b[0]:
+                return 1
+            if a[0] < b[0]:
+                return -1
+            if a[1] > b[1]:
+                return 1
+            if a[1] < b[1]:
+                return -1
+            else:
+                return 0
+
+        local_ordering = sorted(local_ordering, 
+                                key=cmp_to_key(lexicographical_sort))
+        
+        # }}}
+
+        # {{{ construct new node set and global ids
+
+        node_tol = 1e-14
+        global_id = 0
+        global_to_local = []
+        new_nodes = []
+        total_nodes = nnodes * nelts
+        for i in range(total_nodes-1):
+            global_to_local.append((global_id, local_ordering[i]))
+
+            pt1 = local_ordering[i][1]
+            pt2 = local_ordering[i+1][1]
+            if la.norm(pt1 - pt2) > node_tol:
+                new_nodes.append(pt1)
+                global_id += 1
+
+        new_nodes.append(local_ordering[-1][1])
+        new_nodes = np.array(new_nodes).T
+        global_to_local.append((global_id, local_ordering[-1]))
+
+        # }}}
+
+        # {{{ return to local ordering and snatch global indices
+
+        def local_sort(a, b):
+            """
+            Return to local ordering
+            """
+            a = a[1][0]
+            b = b[1][0]
+
+            if a > b:
+                return 1
+            elif a < b:
+                return -1
+            else:
+                return 0
+
+        sorted_elts = sorted(global_to_local, key=cmp_to_key(local_sort))
+
+        ipoint = 0
+        global_indices = np.zeros((nelts, nnodes), dtype=int)
+        for ielt in range(nelts):
+            for inode in range(nnodes):
+                global_indices[ielt,inode] = sorted_elts[ipoint][0]
+                ipoint += 1
+
+        # }}}
+
+        self.__global_to_local = global_indices
+        self._nodes = new_nodes
+
+        return global_indices
 
 
     def _boundary_condition_mask(self, nnz):
@@ -160,9 +269,7 @@ class Discretization:
 
         return A
 
-
-
-    
+ 
     def plot_mapped_elements(self):
         plt.plot(self.mesh.vertices[0], self.mesh.vertices[1], '*',
                  c='tab:red', label='Element vertices', zorder=5, markersize=10)
