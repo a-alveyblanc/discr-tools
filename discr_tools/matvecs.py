3f2d8414d4e356f7b72c955fe55e1e52944bfef2
Addison Alvey-Blanco <aja13@illinois.edu>
2025-05-09 09:11:05 -0500

    add matvecs file

---
+++ discr_tools/matvecs.py
@@ -0,0 +1,18 @@
+import discr_tools.kernels as knl
+
+
+def poisson_matvec(queue, u, discr):
+    pass
+
+
+def mass_matvec(queue, u, discr):
+    pass
+
+
+def grad_matvec(queue, u, discr):
+    pass
+
+
+def div_matvec(queue, u, discr):
+    pass
+
